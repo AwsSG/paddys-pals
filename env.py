fd12efedc67f000999d5537bbd33ea72d20a5107
AwsSG <aws.sabah.gheni@gmail.com>
2021-03-06 23:48:52 +0000

    Merged Borna work on main.html

---
+++ env.py
@@ -0,0 +1,7 @@
+import os
+
+os.environ.setdefault("IP", "0.0.0.0")
+os.environ.setdefault("PORT", "5000")
+os.environ.setdefault("SECRET_KEY", "the-boys-rule!")
+os.environ.setdefault("GOOGLE_API_KEY",
+                      "AIzaSyAC4Sup-rzGPpujeTgUgVMto2f9OydJGAc")
